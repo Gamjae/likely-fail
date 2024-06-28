e5d4eb997f50f4562891ccade5e3d0967c334a3e
DESKTOP-LCI4C1H\dnjs3 <dnjs3214@grayzip.com>
2024-06-28 11:41:07 +0900

    Rename root directory

--- likely-fail/src/main/java/org/gamjae/makitfail/config/AutoConfig.java
+++
@@ -1,9 +0,0 @@
-package org.gamjae.makitfail.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("org.gamjae.makitfail")
-public class AutoConfig {
-}
--- likely-fail/src/main/java/org/gamjae/makitfail/utils/ExceptionThrowUtil.java
+++
@@ -1,8 +0,0 @@
-package org.gamjae.makitfail.utils;
-
-public class ExceptionThrowUtil {
-    public static void throwRandomException(Class<? extends Throwable>[] exceptions) throws Throwable {
-        int randomIndex = (int) (Math.random() * exceptions.length);
-        throw exceptions[randomIndex].getConstructor().newInstance();
-    }
-}
